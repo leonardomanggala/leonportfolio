fbbc6a0dbde3c2f1a485ba9bdaaf08dbb1f1a5d3
leonardomanggala <leonardomanggala@gmail.com>
2024-02-20 13:30:29 +0700

    update tittle

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Leonportfolio",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -144,5 +144,5 @@ export const SOCIALS: SocialObjects = [
     linkTitle: `${SITE.title} on Mastodon`,
     active: false,
   },
-  
+
 ];
